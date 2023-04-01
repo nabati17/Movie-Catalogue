a57057a0aa25e842c54fa306826bc0350be05685
nabati17 <asnaba93@gmail.com>
2023-04-01 13:06:10 +0700

    Application Shell & Web App Manifest

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '256b17a6cfebf128b0f6f9b192890a45',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
